2ea77228975d45a37040139b6dc1bb450bb8d6c9
bld_jmsmachado <joaomiguelmachado@boldint.com>
2017-11-19 19:18:26 +0000

    Adds HashSet add TCs

---
+++ test/com/perfectmatch/domain/collections/set/hashset/MyHashSetAddMusicTest.java
@@ -0,0 +1,63 @@
+package com.perfectmatch.domain.collections.set.hashset;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.perfectmatch.domain.Music;
+import com.perfectmatch.domain.MusicStyle;
+import com.perfectmatch.domain.collections.MusicCollectionsCommon;
+
+/**
+ *  My HashSet AddMusicTest
+ *
+ */
+public class MyHashSetAddMusicTest extends MyHashsetBaseTest {
+
+
+    /**
+     *
+     */
+    private static final String BOB_MUSIC_INSTANCE_SHOULD_BE_EQUAL = "Bob Music instance SHOULD be equal";
+
+    /**
+     *
+     */
+    private static final String THE_ELEMENT_SHOULD_NOT_BE_ADDED_TO_THE_LIST = "The element should NOT be added to the list";
+
+    /**
+    *
+    */
+    private static final String THE_ELEMENT_SHOULD_BE_ADDED_TO_THE_LIST = "The element should be added to the list";
+
+    @Test
+    public void HashSetOfMusicAddsNewMusic() {
+
+        // Given
+        Music bob = new Music(MusicCollectionsCommon.BOB_MARLEY, MusicCollectionsCommon.NO_WOMAN_NO_CRY, MusicStyle.HOUSE);
+        int size = set.size();
+
+        // When
+        boolean addedBob = set.add(bob); // the element exists, therefore do not
+                                         // added it
+
+        // Then
+        assertFalse(THE_ELEMENT_SHOULD_NOT_BE_ADDED_TO_THE_LIST, addedBob);
+        assertEquals(size, set.size());
+    }
+
+    @Test
+    public void HasSetOfMusicAddsNullMusic() {
+
+        // Given
+
+        // When
+        boolean addedBob = set.add(null);
+
+        // Then
+        assertTrue(THE_ELEMENT_SHOULD_BE_ADDED_TO_THE_LIST, addedBob);
+    }
+
+}
---
+++ test/com/perfectmatch/domain/collections/set/hashset/MyHashsetBaseTest.java
@@ -0,0 +1,30 @@
+package com.perfectmatch.domain.collections.set.hashset;
+
+import java.util.Set;
+
+import org.junit.Before;
+
+import com.perfectmatch.domain.Music;
+import com.perfectmatch.domain.collections.MusicCollectionsCommon;
+
+/**
+ * Base class to reuse Commons data structures
+ *
+ * Set do not accept null values
+ * 
+ * Put/Get are BigO(1)
+ * 
+ * 
+ *
+ */
+public class MyHashsetBaseTest {
+
+    protected Set<Music> set;
+
+    @Before
+    public void setUp() {
+
+        set = MusicCollectionsCommon.geBobHashSetCollection();
+    }
+
+}
